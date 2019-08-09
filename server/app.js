74ab5231eb09111723e06177799213d767950627
vivxz <viviannquan@gmail.com>
2019-08-09 16:24:31 -0700

    Created routes for sign up and log in pages

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const partials = require('express-partials');
 const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
 const models = require('./models');
+const db = require('./db');
 
 const app = express();
 
@@ -17,66 +18,117 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+
+// if user is signing up for username and password
+app.get('/signup',
+  (req, res) => {
+    res.render('signup');
+  });
+
+app.post('/signup', (req, res, next) => {
+  var { username, password } = req.body;
+  models.Users.get({ username })
+    .then((result) => {
+      if (!result) {
+        models.Users.create({ username, password })
+          .then(() => res.status(201).redirect('/').send('Created user profile!'))
+          .catch(err => res.status(401).send(err))
+      } else {
+        res.redirect('/signup');
+      }
+    })
+});
+
+
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
+
+app.post('/login', (req, res, next) => {
+  var { username, password } = req.body;
+  // if user doesn't exist
+  // redirect to login page
+  // else
+  // if the attemtpted password matches the password
+  // redirect to home page
+  // else
+  // redirect to login page
+  models.Users.get({ username })
+    .then((result) => {
+      if (!result) {
+          res.redirect('/login');
+      } else {
+        if (models.Users.compare(password, result.password, result.salt)) {
+          res.redirect('/');
+        } else {
+          res.redirect('/login');
+        }
+      }
+    })
+    .catch(err => console.error(err));
+});
+
 
 
 
