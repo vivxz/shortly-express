8ecec98854ac832b5737aff47a2720168054ccf5
vivxz <viviannquan@gmail.com>
2019-08-09 17:38:21 -0700

    Implemented parse cookie method

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  var cookieObject = {};
+  if (req.headers.cookie){
+    var cookieArray = req.headers.cookie.split('; ');
+    cookieArray.forEach((cooks) => {
+      var cookieArr = cooks.split('=');
+      cookieObject[cookieArr[0]] = cookieArr[1];
+    })
+  }
+  req.cookies = cookieObject;
+  next();
 };
 
 module.exports = parseCookies;
