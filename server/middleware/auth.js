7c49ff9a6910a7ed23bc0fb8ad0df65d3469c5fe
vivxz <viviannquan@gmail.com>
2019-08-10 12:29:30 -0700

    Met conditions if no cookies in session or if session already existed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,34 +6,34 @@ module.exports.createSession = (req, res, next) => {
 
   // create new session set to empty object
   // if session does not exist
-    // create hash set to req.cookies.shortlyid
-    // create new session
-    console.log('FUNCTION IS HITTING!!!!!!!!');
-  req.session = {};
+  // create hash set to req.cookies.shortlyid
+  // create new session
   if (Object.keys(req.cookies).length === 0) {
+    req.session = {};
     models.Sessions.create()
-
       .then((result) => {
-        models.Sessions.get({id: result.insertId})
-
+        models.Sessions.get({ id: result.insertId })
         .then((result) => {
-          console.log('RESULT!!', result);
           // take result.hash and assign to session object
+          req.session.hash = result.hash;
           // assign same hash as shortlyid cookie value in req.cookies
+          res.cookie('shortlyid', result.hash);
+          next();
         })
-        .catch((err) => console.log(err));
-      })
-
+      .catch ((err) => console.error(err));
+    })
+  } else {
+    // console.log('req', req); // has cookies
+    req.session = {'hash': ''};
+    models.Sessions.get({ hash: req.cookies.shortlyid })
+     .then((result) => {
+       req.session.hash = result.hash;
+       next();
+     })
+     .catch((err) => console.error(err))
   }
-    // .then((result) => {
-    //   console.log('Result is here here here',result);
-    //   if (!result) {
-    //     var hash = req.cookies.shortlyid;
-    //   }
-    // })
-    // .catch(err => console.log('err))
-  next();
 };
+
 
 /************************************************************/
 // Add additional authentication middleware functions below
